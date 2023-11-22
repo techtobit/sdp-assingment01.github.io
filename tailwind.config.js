fae53922178dd64c138cafcb488354cc852e52f8
codify <nurthern2016@gmail.com>
2023-11-22 22:29:49 +0600

    sdp-assinment

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'hero-pattern': "url('./resources/heroBg.png')",
+      }
+    },
+  },
+  plugins: [],
+}
+
